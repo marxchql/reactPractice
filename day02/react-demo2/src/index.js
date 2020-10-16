5982c7e1863455d7a1426427dbee6736b9597236
pudge-w <pudge_wj@163.com>
2020-10-16 11:21:26 +0800

    渲染数据

--- day02/react-demo2/src/index.js
+++ day02/react-demo2/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import App from './01-setState/State.jsx';
-import App from './02-state-up/App.jsx';
+// import App from './02-state-up/App.jsx';
+// import App from './03-controled-component/App.jsx';
+import App from './04-data-render/App.jsx';
 
 ReactDOM.render(
   <App />,
