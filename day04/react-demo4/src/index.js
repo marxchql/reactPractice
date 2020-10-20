0c0e65d9d647c3c44a07e3545a2d3b0a26186e8e
pudge-w <pudge_wj@163.com>
2020-10-20 11:17:10 +0800

    高阶组件和portal

---
+++ day04/react-demo4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App from './01-demo/App.jsx';
+// import App from './02-hoc/App.jsx';
+import App from './03-portal/App.jsx';
+
+ReactDOM.render(
+  <App msg2="world"/>,
+  document.getElementById('root')
+);
